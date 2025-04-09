130b915d1555406536cea4d7b44ecc0751303462
Niyakit <niyakit123@gmail.com>
2025-04-09 21:38:12 +0700

    add more logs

--- Fetch_Load_to_GCS/main.py
+++ Fetch_Load_to_GCS/main.py
@@ -49,6 +49,8 @@ async def get_match_ids(puuid, server):
     region = server_to_region.get(server, "americas")
 
     summoner_url = f"https://{region}.api.riotgames.com/tft/match/v1/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={RIOTAPIKEY}"
+
+    logger.info(f"Fetching match IDs from {summoner_url}")
     try:
         match_ids = requests.get(summoner_url)
     except requests.exceptions.RequestException as e:
